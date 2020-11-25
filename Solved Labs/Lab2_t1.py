375b3e31a02ee4735553820f697d0e1df2864cdd
jaguargirl <61291322+jaguargirl@users.noreply.github.com>
2020-11-25 17:39:03 +0200

    Update Lab2_t1.py

--- Solved Labs/Lab2_t1.py
+++ Solved Labs/Lab2_t1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m = int(input("Introduceti m= "))
+m = int(input("Introduce m= "))
 t = np.random.random((m, 1))
 b = np.random.random((m, 1))
 A = np.ones((m, 1))
@@ -11,7 +11,7 @@ x = np.linalg.solve(np.dot(A.T, A), np.dot(A.T, b))
 
 t1 = np.linspace(min(t), max(t), 20)
 err = np.linalg.norm(np.dot(A, x)-b)**2
-print("Eroare: ",err)
+print("Error: ",err)
 
 x = x[::-1]
 y = np.polyval(x, t1)
@@ -20,7 +20,7 @@ plt.plot(t, b, "ro")
 plt.plot(t1, y)
 plt.xlabel("t")
 plt.ylabel("b")
-plt.title("Regresie liniara univariata")
-plt.legend(["Datele noastre", "Dreapta de regresie"])
+plt.title("Univariate liniar regression")
+plt.legend(["Our data", "Regression line"])
 plt.show()
 
